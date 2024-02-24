85d1b2d8eec4c878c4155d3c809fe51ee43069f3
Parayll Simnica <parayll.simnica@outlook.com>
2024-02-24 11:00:16 +0100

    Added Model

---
+++ ManagerService/Models/ManagerModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagerService.Models
+{
+    public class ManagerModel
+    {
+        public ManagerModel()
+        {
+            
+        }
+        [Key]
+        public required int ID { get; set; }
+        public required String LastName { get; set; }
+        public required String FirstName { get; set; }
+        public required String Email { get; set; }
+        public required String Password { get; set; }
+        public required String Gender { get; set; }
+        public required DateTime BirthDate { get; set; }
+        public long PID { get; set; }
+        public long RID { get; set; }
+        public readonly String Role = "Admin";
+    }
+}
