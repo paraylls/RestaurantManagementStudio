da548139bd1be7abd6564c3ed9ff653a6acfb1bf
Parayll Simnica <parayll.simnica@outlook.com>
2024-03-24 15:00:43 +0100

    Change Route on BarService

--- RestaurantManagementStudio/BarService/Controllers/BartenderController.cs
+++ RestaurantManagementStudio/BarService/Controllers/BartenderController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BarService.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/bar/[controller]")]
     [ApiController]
     public class BartenderController : ControllerBase
     {
